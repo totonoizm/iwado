000c0e34f95de590bf7df66ed9b04202c4ed4899
Takeshi Ota <singer.scottie@gmail.com>
2020-11-12 14:32:48 +0900

    画像投稿機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5f5e26b35ecae5e33c896e48d14b8dc1721c56dfcdfa570869189a256277b85b8fbf4e42ea9dd9f287718deb7e50102aaa1b544f56b4ab214945c46582cbcc1a'
